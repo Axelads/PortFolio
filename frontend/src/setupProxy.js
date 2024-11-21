5f49bbcf0d81e6f4fc09d998f6523266c1c64af0
Axel Gregoire <professionnel.agregoire@gmail.com>
2024-11-21 15:27:36 +0100

    changement target

--- frontend/src/setupProxy.js
+++ frontend/src/setupProxy.js
@@ -3,7 +3,7 @@ const { createProxyMiddleware } = require('http-proxy-middleware');
 module.exports = function (app) {
   app.use(
     createProxyMiddleware('/api', {
-      target: 'http://localhost:5001', // Adresse de ton backend
+      target: 'https://port-folio-front.vercel.app/', // Adresse de ton backend
       changeOrigin: true,
     })
   );
